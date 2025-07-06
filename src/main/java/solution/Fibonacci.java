0d929ca84b967222f210704cbfc1d07d3f62a975
Gabriel Lins <gabriellinsalvesdonascimento@gmail.com>
2025-07-06 15:12:54 -0300

    add fibonnaci method

--- src/main/java/solution/Fibonacci.java
+++ src/main/java/solution/Fibonacci.java
@@ -12,4 +12,11 @@ public class Fibonacci {
         // Default constructor
         // Do not change it
     }
+
+    public int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
